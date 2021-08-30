8645e6370e8f75ce9b6875c41746b4b7dbe76db9
Angus <angusoneill972@gmail.com>
2021-08-30 19:07:42 +1000

    New method for decoding feed

--- Methods/Feed/DecodeData.java
+++ Methods/Feed/DecodeData.java
@@ -10,31 +10,71 @@ import java.util.List;
 
 public class DecodeData {
 
+    public static String username;
+    public static String msg;
+
     public static int ID;
 
     public static void decode(){
         List<String> raw = Properties.feedData;
-        for(int i = 0; i < raw.size(); i++){
+        Alert.display("raw size "+raw.size());
+
+        for(int i = 0; i < raw.size(); i++) {
             String current = raw.get(i);
-            Integer[] startEnd = new Integer[2];
-            char[] chars = (current.toCharArray());
-            boolean startFound = false;
-            for(int j = 0; j < chars.length; j++){
-                if(chars[j] == '`' && !startFound){
-                    Alert.display("found start at " + j);
-                    startEnd[0] = j;
-                    startFound = true;
-                }else{
-                    if(chars[j] == '`' && startFound){
-                        Alert.display("found end at " + j);
-                        startEnd[1] = j;
-                    }
+            Alert.display("Raw - " + current);
+            boolean found1 = false;
+            boolean found2 = false;
+            int f1 = 0;
+            int f2 = 0;
+            int[] range = new int[2];
+            char[] chars = current.toCharArray();
+
+            // Find username
+
+
+            for (int j = 0; j < chars.length; j++) {
+                if (chars[j] == 'ç' && !found1) {
+                    f1 = j;
+                    found1 = true;
+                } else if (chars[j] == 'ç' && found1 && !found2) {
+                    f2 = j;
+                    found2 = true;
                 }
-                Alert.display(" Made it");
-                Alert.display("" + startEnd[0] + " ");
-                String idString = current.substring(startEnd[0], startEnd[1]);
-                Alert.display(idString);
+                if (found1 && found2) {
+                    range[0] = f1 + 1;
+                    range[1] = f2;
+
+                    username = current.substring(range[0], range[1]);
+
+                    Alert.display("Found username: " + username);
+
+                    found1 = false;
+                    found2 = false;
+                }
             }
+
+            for (int j = 0; j < chars.length; j++) {
+                if (chars[j] == 'Ø' && !found1) {
+                    f1 = j;
+                    found1 = true;
+                } else if (chars[j] == 'Ø' && found1 && !found2) {
+                    f2 = j;
+                    found2 = true;
+                }
+                if (found1 && found2) {
+                    range[0] = f1 + 1;
+                    range[1] = f2;
+
+                    msg = current.substring(range[0], range[1]);
+
+                    Alert.display("Found msg: " + msg);
+
+                    found1 = false;
+                    found2 = false;
+                }
+            }
+
+
         }
     }
 }
